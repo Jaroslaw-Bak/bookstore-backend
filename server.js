1a032b9da8f1c1cd42e9464d4e53ca6137d7a5a3
Jarosław Bąk <jarodelphi@gmail.com>
2023-01-25 00:09:09 +0100

    vercel fix2

--- server.js
+++ server.js
@@ -5,10 +5,7 @@ dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace(
-	'<PASSWORD>',
-	process.env.DATABASE_PASSWORD
-);
+const DB = process.env.DATABASE
 
 async function dbConnect() {
 	await mongoose.set('strictQuery', true).connect(DB);

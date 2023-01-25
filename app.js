3dcdc77b329dc586efbfb5927c9096abbea51f14
Jarosław Bąk <jarodelphi@gmail.com>
2023-01-25 01:31:24 +0100

    cors fix23

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const productRouter = require('./routes/productRoutes');
 const userRouter = require('./routes/userRoutes');
 
 let corsOptions = {
-	origin: ["http://localhost:3000", "bookstore-frontend-m8xepq94d-jaroslaw-bak.vercel.app"],
+	"Access-Control-Allow-Origin": ["http://localhost:3000", "bookstore-frontend-m8xepq94d-jaroslaw-bak.vercel.app"],
   };
 //MIDDLEWARE
 app.use(cors(corsOptions));
